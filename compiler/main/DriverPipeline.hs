1415e9eb0478ada3543fe99a03244e95684f7265
Tamar Christina <tamar@zhox.com>
2016-09-10 13:50:43 +0100

    T10352: code cleanup and clarifications

--- compiler/main/DriverPipeline.hs
+++ compiler/main/DriverPipeline.hs
@@ -1636,7 +1636,6 @@ mkExtraObjToLinkIntoBinary dflags = do
       text "extern StgClosure ZCMain_main_closure;",
       text "int main(int argc, char *argv[])",
       char '{',
-      text " rtsSupportsBoundThreads();",
       text " RtsConfig __conf = defaultRtsConfig;",
       text " __conf.rts_opts_enabled = "
           <> text (show (rtsOptsEnabled dflags)) <> semi,
